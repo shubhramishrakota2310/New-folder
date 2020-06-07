66f10cb895f39b2a32b91aeff9bdc4fb01a8c03e
shubhra2310 <shubhramishrakota2018@gmail.com>
2020-06-07 20:52:52 +0530

    register page mobile version styled

--- register.php
+++ register.php
@@ -4,39 +4,86 @@
 <html>
 <head>
 <title>Register | TFPS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="main.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-	<h2>REGISTER</h2>
+<div class="topnav" id="myTopnav">
+	<a href="#"><img src="TFPS_Logo-1.png" alt=""></a>
+	<a href="register.php"><h3>Register</h3></a>
+	<a href="equipments.php"><h3>View Equipments</h3></a>
+	<a href="./qrDNT"><h3>Borrow </h3></a>
+	<a href="javascript:void(0);" class="icon"
+	onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+<div class="hero-bg">
+	<section class="top">
+
+		</p>
+		<section class="authentic">
+				
+			<img src="bg.svg" alt="">
+		</section>
+	<div class="form-container">
 	<form method="post" action="register.php">
 	<?php include('errors.php'); ?>
 		<div class="input-group">
-			<label>Username</label>
-			<input type="text" name="username" placeholder="Be Choosy!">
+			<input type="text" name="username" placeholder="Username">
 		</div>
 		<div class="input-group">
-			<label>Email</label>
-			<input type="email" name="email" value="<?php echo $email; ?>">
+			<input type="email" name="email" value="<?php echo $email; ?>" placeholder="Email">
 		</div>
 		<div class="input-group">
-			<label>Contact Number</label>
-			<input type="number" name="mobile">
+			<input type="number" name="mobile" placeholder = "Contact Number">
 		</div>
 		<div class="input-group">
-			<label>Password</label>
-			<input type="password" name="passcode" placeholder="Don't Forget it">
+			<input type="password" name="passcode" placeholder="Password">
 		</div>
 		 <div class="input-group">
-			<label>Name</label>
-			<input type="text" name="name" placeholder="Your Name">
+			<input type="text" name="name" placeholder="Name">
 		</div>
     	<div class="input-group">
-			<label>Institute Roll Number</label>
-			<input type="text" name="rollno">
+			<input type="text" name="rollno" placeholder = "Institute Roll Number">
 		</div>
 		<div class="input-group">
-			<button type="submit" class="btn" name="reg_user">Lets Go</button>
+			<input type="button" class="btn" name="reg_user" value="Sign up">
 		</div>
 	</form>
+	</div>
+	</section>
+</div>
+
+<script>
+
+let btn= document.getElementById('cda-btn');
+
+btn.addEventListener('click', () => {
+	 
+
+})
+
+
+
+</script>
+
+
+
+
+<script>
+	function myFunction() {
+	  var x = document.getElementById("myTopnav");
+	  if (x.className === "topnav") {
+		x.className += " responsive";
+	  } else {
+		x.className = "topnav";
+	  }
+	}
+	</script>
+
 
 </body>
 </form>
