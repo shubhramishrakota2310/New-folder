ed9b7cb38975613f51a880b693a1135310103183
shubhra2310 <shubhramishrakota2018@gmail.com>
2020-07-04 15:33:34 +0530

    Nav Bar changed

--- equipments2.php
+++ equipments2.php
@@ -8,19 +8,40 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="navbar.css">
+
+	<script
+    src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+    crossorigin="anonymous"></script>
+    <script>
+    $(function() {
+    $(".toggle").on("click", function() {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+        } else {
+            $(".item").addClass("active");
+        }
+    });
+});
+    </script>
+
+
 </head>
 <body>
 
-<div class="topnav" id="myTopnav">
-    
-    <a href="equipments2.php"><h3>View Equipments</h3></a>
-    <a href="./qrDNT"><h3>Borrow </h3></a>
-    <a href="register.php"><h3>Sign up</h3></a>
-    <a href="login.php"><h3>Sign in</h3></a>
-    <a href="javascript:void(0);" class="icon"
-    onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
+<nav>
+        <ul class="menu">
+            <li class="logo"><a href="index.html"><img src="Group 5.png" alt=""></a></li>
+            <li class="item"><a href="equipments2.php">View Equipments</a></li>
+            <li class="item"><a href="./qrDNT">Borrow</a></li>
+            </li>
+            <li class="item button"><a href="login.php">Log In</a></li>
+            <li class="item button secondary"><a href="register.php">Sign Up</a></li>
+            <li class="toggle"><span class="bars"></span></li>
+        </ul>
+    </nav>
 </div>
 <div class="w3-container" style="overflow-x: auto;">
 	<h5 style="margin-left: 20px; display: inline-block;">View Equipments</h5>
@@ -69,14 +90,7 @@
           }
         }
 
-    function myFunction() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
-        x.className += " responsive";
-      } else {
-        x.className = "topnav";
-      }
-    }
+   
     </script>
 
 
--- qrDNT/index.php
+++ qrDNT/index.php
@@ -7,18 +7,37 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="../navbar.css">
+
+	<script
+    src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+    crossorigin="anonymous"></script>
+    <script>
+    $(function() {
+    $(".toggle").on("click", function() {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+        } else {
+            $(".item").addClass("active");
+        }
+    });
+});
+    </script>
+</head>
 <body>
 <div class="container">
-<div class="topnav" id="myTopnav">
-	<a href="../equipments.php"><h3>View Equipments</h3></a>
-	<a href="../qrDNT"><h3>Borrow </h3></a>
-	<a href="../register.php"><h3>Sign up</h3></a>
-	<a href="../login.php"><h3>Sign in</h3></a>
-	<a href="javascript:void(0);" class="icon"
-	onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
-</div>
+<nav>
+        <ul class="menu">
+            <li class="logo"><a href="../index.html"><img src="Group 5.png" alt=""></a></li>
+            <li class="item"><a href="../equipments2.php">View Equipments</a></li>
+            <li class="item"><a href="index.php">Borrow</a></li>
+            </li>
+            <li class="item button"><a href="../login.php">Log In</a></li>
+            <li class="item button secondary"><a href="../register.php">Sign Up</a></li>
+            <li class="toggle"><span class="bars"></span></li>
+        </ul>
+    </nav>
     <h4> <span>Scan the QR code </span> of the Equipment</h4>
 
 <div class="row">
